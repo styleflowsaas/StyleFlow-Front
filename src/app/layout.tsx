8c15b0fa0db265777af942968eb769b8aba59b4e
Victor <estudiodemenezes@gmail.com>
2024-10-12 23:40:02 -0300

    Layout and css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "sonner";
+import NavBar from "@/components/NavBar/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,9 +28,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased grid`}
       >
-        {children}
+        <div className="grid grid-cols-[auto_1fr] min-h-screen text-[1em]">
+          {/* Barra de navegación fija */}
+          <NavBar />
+
+          {/* Contenido dinámico */}
+          <main className="p-1">{children}</main>
+        </div>
         <Toaster
           position="top-center"
           expand={false}
