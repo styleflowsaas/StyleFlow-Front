21a8d6b3d7f3e3e3af351b56f0214cbdb6e2d195
Victor <estudiodemenezes@gmail.com>
2024-10-14 11:23:29 -0300

    table responsive mobile

--- src/components/Table/ProductTable.tsx
+++ src/components/Table/ProductTable.tsx
@@ -7,32 +7,55 @@ interface ProductTableProps {
 
 const ProductTable: React.FC<ProductTableProps> = ({ data }) => {
   return (
-    <table className="text-black border table-fill ">
-      <thead>
-        <tr className="shadow-lg">
-          <th className="text-left">Producto</th>
-          <th className="text-left">Marca</th>
-          <th className="text-left">Descripción</th>
-          <th className="text-left">Categoría</th>
-          <th className="text-left">Precio</th>
-          <th className="text-left">Acción</th>
-        </tr>
-      </thead>
-      <tbody className="table-hover">
-        {data.map((product) => (
-          <tr key={product.id} className="hoverTd">
-            <td className="text-left">{product.name}</td>
-            <td className="text-left">{product.brand}</td>
-            <td className="text-left">{product.description}</td>
-            <td className="text-left">{product.category}</td>
-            <td className="text-left">$ {product.price}</td>
-            <td className="text-left">
-              <Link href={`/products/${product.id}`}>Ver Imagen</Link>
-            </td>
+    <>
+      <table className="text-black border table-fill hidden md:table ">
+        <thead>
+          <tr className="shadow-lg">
+            <th className="text-left">Producto</th>
+            <th className="text-left">Marca</th>
+            <th className="text-left">Descripción</th>
+            <th className="text-left">Categoría</th>
+            <th className="text-left">Precio</th>
+            <th className="text-left">Acción</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="table-hover">
+          {data.map((product) => (
+            <tr key={product.id} className="hoverTd">
+              <td className="text-left">{product.name}</td>
+              <td className="text-left">{product.brand}</td>
+              <td className="text-left">{product.description}</td>
+              <td className="text-left">{product.category}</td>
+              <td className="text-left">$ {product.price}</td>
+              <td className="text-left">
+                <Link href={`/products/${product.id}`}>Ver Imagen</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table className="md:hidden">
+        <thead>
+          <tr className="shadow-lg">
+            <th className="text-left">Producto</th>
+            <th className="text-left">Precio</th>
+            <th className="text-left">Acción</th>
+          </tr>
+        </thead>
+        <tbody className="table-hover">
+          {data.map((product) => (
+            <tr key={product.id} className="hoverTd">
+              <td className="text-left">{product.name}</td>
+
+              <td className="text-left">$ {product.price}</td>
+              <td className="text-left">
+                <Link href={`/products/${product.id}`}>Ver Producto</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
