c4564c6676e4e75305b432b686f84a1247d20d00
Victor <estudiodemenezes@gmail.com>
2024-10-13 21:15:08 -0300

    collapse button on navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import DarkModeToggle from "../buttons/Dark";
 import Link from "next/link";
-import { MdArrowBack, MdArrowForward } from "react-icons/md";
+import HamburgerButton from "../buttons/BurgerButton";
 
 const NavBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,88 +13,73 @@ const NavBar: React.FC = () => {
     setIsCollapsed(!isCollapsed);
   };
   return (
-    <div className="flex flex-col">
-      <nav
-        className={`flex flex-col  ${isCollapsed ? "w-0 " : "w-[15vw]"} ${
-          isOpen ? "w-[50vw] fixed" : "w-0 max-w-[15vw]"
-        } gap-2 items-center justify-between bg-secundario dark:bg-fondo-dark fixed p-4 h-screen transition-all duration-300 z-30`}
+    <nav
+      className={`flex flex-col  ${isCollapsed ? "w-0 " : "w-[15vw]"} ${
+        isOpen ? "w-[50vw] fixed" : "w-0 max-w-[15vw]"
+      } gap-2 items-center justify-between bg-secundario dark:bg-fondo-dark p-4 min-h-screen transition-all duration-300 z-30`}
+    >
+      {/* Botón para colapsar la barra */}
+      <div className="hidden md:block fixed z-40 -top-1 -left-1">
+        <HamburgerButton toggle={toggleCollapse} isCollapse={isCollapsed} />
+      </div>
+      <div className="flex flex-col gap-2 justify-evenly items-center w-full md:w-auto">
+        <button
+          className="md:hidden text-white"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </button>
+        <Image
+          src={"/logoDark.svg"}
+          alt="logo"
+          width={100}
+          height={100}
+          className={`w-full ${isOpen ? "" : "hidden md:block"}`}
+        />
+      </div>
+
+      <ul
+        className={`flex flex-col gap-2 items-center text-base md:text-xs transition-transform md:flex ${
+          isOpen ? "flex" : "hidden"
+        } ${isCollapsed ? "opacity-100 hidden" : ""}`}
       >
-        {/* Botón para colapsar la barra */}
-        <button
-          onClick={toggleCollapse}
-          className="hidden md:absolute top-1 left-1 rounded-full text-sm z-40"
-        >
-          {isCollapsed ? <MdArrowForward /> : <MdArrowBack />}
-        </button>
-        <div className="flex justify-evenly items-center w-full md:w-auto">
-          <Image
-            src={"/logoDark.svg"}
-            alt="logo"
-            width={100}
-            height={100}
-            className={`w-1/3 md:w-full ${isOpen ? "" : "hidden md:block"}`}
-          />
-          <button
-            className="md:hidden text-white"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-              />
-            </svg>
-          </button>
+        <Link href="/Ventas" className={`${isCollapsed ? "hidden" : ""}`}>
+          Ventas
+        </Link>
+        <Link href="/Productos" className={`${isCollapsed ? "hidden" : ""}`}>
+          Productos
+        </Link>
+        <Link href="/Proveedores" className={`${isCollapsed ? "hidden" : ""}`}>
+          Proveedores
+        </Link>
+        <Link href="/Informes" className={`${isCollapsed ? "hidden" : ""}`}>
+          Informes
+        </Link>
+        <Link href="/MiCuenta" className={`${isCollapsed ? "hidden" : ""}`}>
+          Mi Cuenta
+        </Link>
+        <div className={`${isCollapsed ? "hidden" : ""}`}>
+          <DarkModeToggle />
         </div>
+      </ul>
 
-        <ul
-          className={`flex flex-col gap-2 items-center text-base md:text-xs transition-transform md:flex ${
-            isOpen ? "flex" : "hidden"
-          } ${isCollapsed ? "opacity-100 hidden" : ""}`}
-        >
-          <Link href="/Ventas" className={`${isCollapsed ? "hidden" : ""}`}>
-            Ventas
-          </Link>
-          <Link href="/Productos" className={`${isCollapsed ? "hidden" : ""}`}>
-            Productos
-          </Link>
-          <Link
-            href="/Proveedores"
-            className={`${isCollapsed ? "hidden" : ""}`}
-          >
-            Proveedores
-          </Link>
-          <Link href="/Informes" className={`${isCollapsed ? "hidden" : ""}`}>
-            Informes
-          </Link>
-          <Link href="/MiCuenta" className={`${isCollapsed ? "hidden" : ""}`}>
-            Mi Cuenta
-          </Link>
-          <div className={`${isCollapsed ? "hidden" : ""}`}>
-            <DarkModeToggle />
-          </div>
-        </ul>
-
-        <button
-          className={`text-base md:text-xs ${isCollapsed ? "hidden" : ""}`}
-        >
-          Salir
-        </button>
-      </nav>
-      <div
-        className={`bg-secundario dark:bg-fondo-dark h-full ${
-          isCollapsed ? "w-0" : "w-[15vw]"
-        }`}
-      ></div>
-    </div>
+      <button className={`text-base md:text-xs ${isCollapsed ? "hidden" : ""}`}>
+        Salir
+      </button>
+    </nav>
   );
 };
 
---
+++ src/components/buttons/BurgerButton.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { MotionConfig, motion } from "framer-motion";
+
+interface HamburgerButtonProps {
+  toggle: (pv: boolean) => void;
+  isCollapse: boolean;
+}
+export const HamburgerButton: React.FC<HamburgerButtonProps> = ({
+  toggle,
+  isCollapse,
+}) => {
+  return (
+    <MotionConfig
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
+      }}
+    >
+      <motion.button
+        initial={false}
+        animate={isCollapse ? "closed" : "open"}
+        onClick={() => toggle(!isCollapse)}
+        className="relative h-10 w-10 rounded-full bg-white/0 transition-colors "
+      >
+        <motion.span
+          variants={VARIANTS.top}
+          className="absolute h-0.5 w-5 bg-white"
+          style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
+        />
+        <motion.span
+          variants={VARIANTS.middle}
+          className="absolute h-0.5 w-5 bg-white"
+          style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
+        />
+      </motion.button>
+    </MotionConfig>
+  );
+};
+
+export default HamburgerButton;
+const VARIANTS = {
+  top: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      top: ["35%", "50%", "50%"],
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      top: ["50%", "50%", "35%"],
+    },
+  },
+  middle: {
+    open: {
+      rotate: ["0deg", "0deg", "-45deg"],
+    },
+    closed: {
+      rotate: ["-45deg", "0deg", "0deg"],
+    },
+  },
+  bottom: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      bottom: ["35%", "50%", "50%"],
+      left: "0%",
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      bottom: ["50%", "50%", "35%"],
+      left: "50%",
+    },
+  },
+};
