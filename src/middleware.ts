c16e25e37473804a4fff6f9b83980e8644804543
Victor <estudiodemenezes@gmail.com>
2024-10-18 23:47:30 -0300

    middleware active again

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+
 export function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
   const token = req.cookies.get("token-sf");
   if (!token) {
     return NextResponse.redirect(new URL("/Sign", req.url));
+  }
+
+  if (token && pathname === "/Sign") {
+    return NextResponse.redirect(new URL("/Ventas", req.url));
   }
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    // "/Informes/:path*",
-    // "/MiCuenta/:path*",
-    // "/Productos/:path*",
-    // "/Proveedores/:path*",
-    // "/Ventas/:path*",
+    "/Informes/:path*",
+    "/MiCuenta/:path*",
+    "/Productos/:path*",
+    "/Proveedores/:path*",
+    "/Ventas/:path*",
   ],
 };
